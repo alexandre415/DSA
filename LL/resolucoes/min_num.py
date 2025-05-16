88497b66aaa9d7e00275427aaf871c2422f85fda
Alexandre Cardoso <alexandre.pradoc@gmail.com>
2025-05-16 15:31:31 -0300

    renomeando pasta

--- LL/resolucoes/min_num.py
+++
@@ -1,6 +0,0 @@
-list = [1,20,10,40]
-print(f'Usando a funcao min {min(list)}')
-
-min = list[0]
-
-#or 
--- LL/resolucoes/reversing_string.py
+++
@@ -1,4 +0,0 @@
-string = "Python Programming"
-# Reversing the string using slicing technique [start:stop:step]
-reversed_string = string[::-1]
-print(reversed_string)
